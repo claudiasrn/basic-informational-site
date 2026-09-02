284999b67baf9315f70bd3add4ce06d12e4fe3fa
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-02 13:49:13 +0200

    refactor: rewrite server routing using express

--- index.js
+++ index.js
@@ -1,58 +1,24 @@
-const http = require("http");
-const fs = require("fs/promises");
+const express = require("express");
 const path = require("path");
 
-const server = http.createServer(async (req, res) => {
-	const myUrl = new URL(req.url, "http://localhost:8080");
+const app = express();
 
-	if (myUrl.pathname === "/") {
-		try {
-			const data = await fs.readFile(
-				path.join(__dirname, "index.html"),
-				"utf-8",
-			);
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.end(data);
-		} catch (err) {
-			res.writeHead(500, { "Content-Type": "text/html" });
-			res.end("Server error");
-		}
-	} else if (myUrl.pathname === "/about") {
-		try {
-			const data = await fs.readFile(
-				path.join(__dirname, "about.html"),
-				"utf-8",
-			);
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.end(data);
-		} catch (err) {
-			res.writeHead(500, { "Content-Type": "text/html" });
-			res.end("Server error");
-		}
-	} else if (myUrl.pathname === "/contact-me") {
-		try {
-			const data = await fs.readFile(
-				path.join(__dirname, "contact-me.html"),
-				"utf-8",
-			);
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.end(data);
-		} catch (err) {
-			res.writeHead(500, { "Content-Type": "text/html" });
-			res.end("Server error");
-		}
-	} else {
-		try {
-			const data = await fs.readFile(path.join(__dirname, "404.html"), "utf-8");
-			res.writeHead(404, { "Content-Type": "text/html" });
-			res.end(data);
-		} catch (err) {
-			res.writeHead(500, { "Content-Type": "text/html" });
-			res.end("Server error");
-		}
-	}
+app.listen(8080, () => {
+	console.log("Server running on port 8080");
+})
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(8080, () => {
-	console.log("Server running on port 8080");
+app.get("/about", (req, res) => {
+	res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+	res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.use((req, res) => {
+	res.status(404).sendFile(path.join(__dirname, "404.html"));
 });
